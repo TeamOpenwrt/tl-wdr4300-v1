65896c459bdb2989afa20ba7d43a834682ca7d9e
BenLue <BenJule@users.noreply.github.com>
2020-03-18 19:41:06 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,14 +11,14 @@ cd ${ROOT_DIR}
 # Install all necessary packages
 #sudo apt-get install build-essential subversion libncurses5-dev zlib1g-dev gawk gcc-multilib flex git-core libssl-dev unzip python wget time
 
-//./scripts/feeds update -a
-//./scripts/feeds install -a
+#./scripts/feeds update -a
+#./scripts/feeds install -a
 ls -lah
 
-cp $ROOT_DIR/build_script/diffconfig $ROOT_DIR/.config
+cp /build_script/diffconfig $ROOT_DIR/.config
 cat diffconfig >> .config
 make defconfig
 
-make clean
+#make clean
 
-make -j$(nproc) || make V=s # Retry with full log if failed
+#make -j$(nproc) || make V=s # Retry with full log if failed
