24cac25688d721e05ed72c83ea1e25480124562c
BenLue <BenJule@users.noreply.github.com>
2020-03-18 21:48:39 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,13 +11,19 @@ cd ${ROOT_DIR}
 # Install all necessary packages
 #sudo apt-get install build-essential subversion libncurses5-dev zlib1g-dev gawk gcc-multilib flex git-core libssl-dev unzip python wget time
 
+#feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-cat $PWD/build_script/diffconfig >> .config
-cat .config
-make defconfig
+#config target
+rm -rf .config
+echo "CONFIG_TARGET_ar71xx=y" > .config
+echo "CONFIG_TARGET_ar71xx_generic=y" > .config
+echo "CONFIG_TARGET_ar71xx_generic_DEVICE_tl-wdr4300-v1=y" > .config
 
-make clean
+make defconfig
+make -j 4 
+
+#make clean
 
 make -j$(nproc) || make V=s # Retry with full log if failed
