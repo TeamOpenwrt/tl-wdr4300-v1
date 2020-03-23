73776a5edbf8904fcff58f328ab6631bcb360296
BenLue <BenJule@users.noreply.github.com>
2020-03-23 16:29:27 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ export PATH=~/bin:$PATH
 SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT_DIR=${SCRIPTS_DIR}/..
 cd ${ROOT_DIR}
+
+make clean
+
 mkdir -p $OPENWRT_CONFIG_DIR
 # Install all necessary packages
 #sudo apt-get install build-essential subversion libncurses5-dev zlib1g-dev gawk gcc-multilib flex git-core libssl-dev unzip python wget time
@@ -15,12 +18,6 @@ mkdir -p $OPENWRT_CONFIG_DIR
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-#echo "CONFIG_TARGET_ar71xx=y" > .config
-#echo "CONFIG_TARGET_ar71xx_generic=y" > .config
-#echo "CONFIG_TARGET_ar71xx_generic_DEVICE_tl-wdr4300-v1=y" > .config
-
-
-#make clean
 cat ${ROOT_DIR}/build_script/diffconfig >> .config
 make defconfig;make oldconfig
 make -j$(nproc) || make V=s # Retry with full log if failed
