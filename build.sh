db00c90ebad57db4a443c59273ad16acb56b2ba1
BenLue <BenJule@users.noreply.github.com>
2020-03-23 16:31:08 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,6 @@ SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT_DIR=${SCRIPTS_DIR}/..
 cd ${ROOT_DIR}
 
-make clean
-
 mkdir -p $OPENWRT_CONFIG_DIR
 # Install all necessary packages
 #sudo apt-get install build-essential subversion libncurses5-dev zlib1g-dev gawk gcc-multilib flex git-core libssl-dev unzip python wget time
@@ -18,6 +16,7 @@ mkdir -p $OPENWRT_CONFIG_DIR
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+# make clean
 cat ${ROOT_DIR}/build_script/diffconfig >> .config
 make defconfig;make oldconfig
 make -j$(nproc) || make V=s # Retry with full log if failed
